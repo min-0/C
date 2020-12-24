db4a62903394810896b00a9eb935645835b022c9
min-0 <66067273+min-0@users.noreply.github.com>
2020-12-24 16:24:59 +0900

    C
    2018년도 C 기출문제

---
+++ C_Practice_1/18년도1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct person
+{
+	char name[10];
+	int score[4];
+	double avg;
+};
+
+void output_info(struct person *team, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%s ", team[i].name);
+		for (int k = 0; k < 4; k++)
+		{
+			printf("%d/", team[i].score[k]);
+		}
+		printf("\b - %.1lf\n", team[i].avg);
+	}
+}
+
+int main()
+{
+	struct person team[5] = {
+		{ .name = "±è±æµ¿", .score[0] = 80, .score[1] = 60, .score[2] = 75, .score[3] = 40 },
+		{ .name = "±è¼¼¿µ", .score[0] = 60, .score[1] = 75, .score[2] = 60, .score[3] = 55 },
+		{ .name = "ÀåÈ«¼ö", .score[0] = 55, .score[1] = 60, .score[2] = 75, .score[3] = 85 },
+		{ .name = "±è¿µÈñ", .score[0] = 50, .score[1] = 80, .score[2] = 90, .score[3] = 85 },
+		{ .name = "¹ÚÁ¾¼ö", .score[0] = 90, .score[1] = 80, .score[2] = 85, .score[3] = 90 }
+	};
+
+	output_info(team, sizeof(team) / sizeof(struct person));
+}
